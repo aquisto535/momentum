fe64d5dca9691b16215810531a025f041d22ba83
Tae Min Jung <outopia3@gmail.com>
2022-09-02 20:26:42 +0900

    make random bgimg
    we are progressing!

--- background.js
+++ background.js
@@ -1,13 +1,10 @@
-const images = [
-    "0.jpg",
-    "1.jpg",
-    "2.jpg"
-]
+const images = ['0.jpg', '1.jpg', '2.jpg'];
 
-const chosenImages = images[Math.floor(Math.random() * images.length)];
+//random으로 이미지 나타내기
+const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
+// 배경 이미지 넣는 형식 url('img주소')
+const bgImage = `url(img/${chosenImage})`;
 
-bgImage.src = `img/${chosenImages}`;
-
-document.body.appendChild(bgImage);
+// body에 추가
+document.body.style.backgroundImage = bgImage;
